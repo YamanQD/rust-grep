14f1c654f43c587a0e89e048a8ae775f4c00319f
Yaman Qassas <yaman102011@gmail.com>
2021-10-09 16:53:09 +0300

    change error messages

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {}", err);
+        println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(error) = rust_grep::run(config) {
-        println!("Error reading file: {}", error);
+        println!("Program error: {}", error);
         process::exit(1);
     }
 }
