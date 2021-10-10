eb5269b2270cccf57489ef320e3aeda204c23478
Yaman Qassas <yaman102011@gmail.com>
2021-10-10 14:26:19 +0300

    redirect errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(error) = rust_grep::run(config) {
-        println!("Program error: {}", error);
+        eprintln!("Program error: {}", error);
         process::exit(1);
     }
 }
